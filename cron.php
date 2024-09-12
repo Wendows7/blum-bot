7ffd1fdfc4965a83d3040bf5596187c7ef5425d9
Arswendo Suryahadi <asswendo@gmail.com>
2024-09-12 18:03:32 +0700

    fix cron

--- cron.php
+++ cron.php
@@ -14,34 +14,34 @@ class Main
             echo "Generate new token" . PHP_EOL;
             $blum->refeshAccessToken();
             echo "Token Refreshed" . PHP_EOL;
-            sleep(5);
-        }
-        $acount = $blum->checkAcount();
-        $balance = isset($acount['availableBalance']) ? $acount['availableBalance'] : "Fail Get Balance";
-        $ticket = isset($acount['playPasses']) ? $acount['playPasses'] : "Fail Get Ticket";
-        echo "Total Balance: " . $balance . PHP_EOL;
-        echo "Total Ticket: " . $ticket . PHP_EOL;
-        $daily = $blum->dailyCheckin();
-        if (isset($daily['message']) && $daily['message'] === "same day") {
-            echo "Checkin Status: Already Checkin" . PHP_EOL;
         } else {
-            echo "Checkin Status: Checkin Success" . PHP_EOL;
-        }
-        if (isset($blum->checkAcount()["farming"])) {
-            $claimFarming = $blum->claimFarming();
-            if (isset($claimFarming["message"]) && $claimFarming["message"] === "It's too early to claim") {
-                echo "Farming Status: Running" . PHP_EOL;
+            $acount = $blum->checkAcount();
+            $balance = isset($acount['availableBalance']) ? $acount['availableBalance'] : "Fail Get Balance";
+            $ticket = isset($acount['playPasses']) ? $acount['playPasses'] : "Fail Get Ticket";
+            echo "Total Balance: " . $balance . PHP_EOL;
+            echo "Total Ticket: " . $ticket . PHP_EOL;
+            $daily = $blum->dailyCheckin();
+            if (isset($daily['message']) && $daily['message'] === "same day") {
+                echo "Checkin Status: Already Checkin" . PHP_EOL;
             } else {
-                echo "Farming Claimed...."  . PHP_EOL;
+                echo "Checkin Status: Checkin Success" . PHP_EOL;
+            }
+            if (isset($blum->checkAcount()["farming"])) {
+                $claimFarming = $blum->claimFarming();
+                if (isset($claimFarming["message"]) && $claimFarming["message"] === "It's too early to claim") {
+                    echo "Farming Status: Running" . PHP_EOL;
+                } else {
+                    echo "Farming Claimed...."  . PHP_EOL;
+                    echo "Farming Status: Stopped" . PHP_EOL;
+                    $blum->startFarming();
+                    echo "Farming Started...."  . PHP_EOL;
+                }
+            } else {
+                $blum->claimFarming();
                 echo "Farming Status: Stopped" . PHP_EOL;
                 $blum->startFarming();
                 echo "Farming Started...."  . PHP_EOL;
             }
-        } else {
-            $blum->claimFarming();
-            echo "Farming Status: Stopped" . PHP_EOL;
-            $blum->startFarming();
-            echo "Farming Started...."  . PHP_EOL;
         }
 
         echo "\nSleeping for 3 hours\n";
